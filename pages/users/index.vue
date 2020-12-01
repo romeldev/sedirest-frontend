6ec7ae3712ca0a39d9257bbcbfc4006de576e716
romel diaz <root.romel@gmail.com>
2020-12-01 00:34:11 -0500

    first commit

---
+++ pages/users/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      hola
+  </div>
+</template>
+
+<script>
+export default {
+
+    data: () => ({
+        resource: 'user',
+        search: '',
+        data: {},
+    }),
+
+    created() {
+        this.getData()
+    },
+
+    methods: {
+        getData( page=1 ){
+            this.$axios.get('/users')
+            .then( res => {
+                data = res.data;
+                console.log(data)
+            }) 
+            .catch( err => {
+                console.log(err.response)
+            })
+            
+        }    
+    },
+
+    
+}
+</script>
+
+<style>
+
+</style>
